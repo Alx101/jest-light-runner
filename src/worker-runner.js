8afb5f981dc5243780f3fc1ad89945dd3160e388
Alex Örnberg <alex.ornberg@demando.se>
2022-12-02 12:18:21 +0100

    fixup! Add coverage support

--- src/worker-runner.js
+++ src/worker-runner.js
@@ -100,7 +100,7 @@ export default async function run({
   }
   
   const result = addSnapshotData(
-    toTestResult(stats, results, test, CoverageInstrumenter),
+    toTestResult(stats, results, test, v8Coverage),
     snapshotState
   );
 
